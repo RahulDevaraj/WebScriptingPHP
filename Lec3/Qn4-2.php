77d2f19e86a5f61f7ca8ebf1f03bac719064c071
RahulDevaraj <kannan.vegeta@gmail.com>
2023-01-25 19:52:50 -0800

    Lec3 Pending

---
+++ Lec3/Qn4-2.php
@@ -0,0 +1,10 @@
+<?php
+$population = array("NY"=>array("New York"=> 8008278),
+"CA"=>array("Los Angeles"=>379621,"San Diego"=>1307407),
+"IL"=>array("Chicago"=>2695598)
+);
+//Show the above array in a table with the following format:
+
+
+
+?>
